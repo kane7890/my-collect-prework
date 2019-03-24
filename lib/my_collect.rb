41a3c9982ca4bb5ec4a6542aa3d84f3143ee4d39
Kane Koo <kanekoo@gmail.com>
2019-03-24 22:40:11 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,13 @@
-
+def my_collect (collecshun)
+  i=0
+  clength=collecshun.length
+  if clength > 0 
+  while i < clength 
+     token=yield(collecshun[i])
+     i += 1
+    end
+  end
+  collecshun
+  # code here
+end
 
